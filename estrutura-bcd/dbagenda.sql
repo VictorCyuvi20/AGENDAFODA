c2710bd06da10286ac82edf8b26958de67a5e047
VictorCyuvi20 <victoramancio2020@gmail.com>
2024-10-29 10:43:17 -0300

    APENAS

---
+++ estrutura-bcd/dbagenda.sql
@@ -0,0 +1,12 @@
+Create database dbagenda;
+
+use dbagenda; 
+
+Create table tbUsuarios (
+	nome VARCHAR(60) NOT NULL,
+	usuario VARCHAR(20) NOT NULL primary key,
+    telefoone VARCHAR(15),
+    senha Varchar(15) NOT NULL
+    );
+    
+    
